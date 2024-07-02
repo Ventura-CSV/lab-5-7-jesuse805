93773978da009b8d185926e747c4a19fb57fd101
jesus <jesus_estrada5@my.vcccd.edu>
2024-07-02 01:11:57 -0700

    CS V11

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
 def splitlist(numbers):
-
+    # finds minimum value
     minimumvalue = min(numbers)
-
+    # index of minimum value
     minIndex = numbers.index(minimumvalue)
-
+    # swaps min value w first element
     numbers[0], numbers[minIndex] = numbers[minIndex], numbers[0]
-
+    # splits list using *args
     first, *others = numbers
 
     return first, others
